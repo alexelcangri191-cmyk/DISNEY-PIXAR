0921dd10b1c363fd3e8660a6b0f2511c2df89111
alexelcangri191-cmyk <alexelcangri191@gmail.com>
2026-06-22 23:15:30 -0500

    Updated Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -237,7 +237,7 @@ export default function Login() {
               onMouseEnter={(e) => (e.currentTarget.style.opacity = '0.8')}
               onMouseLeave={(e) => (e.currentTarget.style.opacity = '1')}
             >
-              Crea una
+              Crea una cuenta
             </Link>
           </p>
         </div>
