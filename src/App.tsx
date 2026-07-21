bbce6c52ddaef763e7e63673910e39792a48d800
alexelcangri191-cmyk <alexelcangri191@gmail.com>
2026-07-20 22:57:33 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import Perfil from './pages/Perfil';
 import UnderConstruction from './pages/UnderConstruction';
 import Recargar from './pages/Recargar';
-import PaginaQR from './pages/PaginaQR';
 import CuentaBancaria from './pages/CuentaBancaria';
 import MiEquipo from './pages/MiEquipo';
 import SobreNosotros from './pages/SobreNosotros';
@@ -25,7 +24,6 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
         <Route path="/recargar" element={<ProtectedRoute><Recargar /></ProtectedRoute>} />
-        <Route path="/recargas/qr" element={<ProtectedRoute><PaginaQR /></ProtectedRoute>} />
         <Route path="/retiros" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
         <Route path="/historial-recargas" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
         <Route path="/historial-retiros" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
