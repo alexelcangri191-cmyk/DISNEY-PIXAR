ec618a2725a5727de716b578beced8b4051b37a9
alexelcangri191-cmyk <alexelcangri191@gmail.com>
2026-07-23 07:39:10 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import FormasDeGanar from './pages/FormasDeGanar';
 import Videos from './pages/Videos';
 import Soporte from './pages/Soporte';
 import Niveles from './pages/Niveles';
-import Retiros from './pages/Retiros';
 import ProtectedRoute from './components/ProtectedRoute';
 
 export default function App() {
@@ -25,7 +24,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/perfil" element={<ProtectedRoute><Perfil /></ProtectedRoute>} />
         <Route path="/recargar" element={<ProtectedRoute><Recargar /></ProtectedRoute>} />
-        <Route path="/retiros" element={<ProtectedRoute><Retiros /></ProtectedRoute>} />
+        <Route path="/retiros" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
         <Route path="/historial-recargas" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
         <Route path="/historial-retiros" element={<ProtectedRoute><UnderConstruction /></ProtectedRoute>} />
         <Route path="/cuenta-bancaria" element={<ProtectedRoute><CuentaBancaria /></ProtectedRoute>} />
